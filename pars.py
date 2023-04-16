dfc9e6781b6a99a1c0234d8e2f3ef7eec179c959
Rec1dite <rec1dite@gmail.com>
2023-04-17 00:45:03 +0200

    Added parser skeleton

--- pars.py
+++ pars.py
@@ -22,6 +22,7 @@ def parse(file):
 
     parser = Parser(text)
     # parser.woodoomagic()
+
     # ast = parse()
     # outputXML(ast, file)
 
@@ -47,24 +48,92 @@ class Parser:
 
     def __init__(self, tokens):
         self.tokens = tokens
-        self.index = 0 # tok
-        self.lookahead = None
+        self.index = 0
+        self.ast = None
     
-    def nextToken(self):
-        if self.lookahead is None:
-            return self.tokens[self.index]
-
-        self.index += 1
+    def currentToken(self):
         return self.tokens[self.index]
     
+    # Test for equality of current token
     def match(self, token):
-        if self.lookahead == token:
-            self.nextToken()
-        else:
-            raise Exception("Syntax error: expected " + token + " but got " + self.lookahead)
+        if self.index >= len(self.tokens):
+            raise Exception("Syntax error: unexpected EOF")
 
-    def parseExpr(self):
-        pass
+        elif self.currToken() == token:
+            self.index += 1
+
+        else:
+            raise Exception("Syntax error: unexpected " + self.currToken() + ", expected " + token)
+
+    def tNode(self, token):
+        return { "token": token }
+    
+    def nNode(self, name, children):
+        return { name: children }
+
+    #=============== GRAMMAR ===============#
+    # Each of these returns a tree node
+
+    def progr_(self):
+        if self.currentToken() in ["h", "g", "c", "w", "i", "r", "o", "n", "b", "s"]:
+            # ALGO PROCDEFS
+
+            tree1 = algo_()
+            tree2 = procdefs_()
+
+            tree = { "PROGR": self.currentToken() }
+
+    def procdefs_(self): pass
+    def proc_(self): pass
+    def digits_(self): pass
+    def d_(self): pass
+    def more_(self): pass
+    def algo_(self): pass
+    def seq_(self): pass
+    def instr_(self): pass
+
+    def call_(self):
+        if self.currentToken() == "c":
+            nodes = []
+
+            self.match("c")
+            nodes.append(self.tNode("c"))
+
+            self.match("p")
+            nodes.append(self.tNode("p"))
+
+            nodes.append(self.digits_())
+
+            return self.nNode("CALL", nodes)
+
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected c")
+
+    def assign_(self): pass
+    def loop_(self): pass
+    def branch_(self): pass
+    def else_(self): pass
+    def numvar_(self): pass
+    def boolvar_(self): pass
+    def stringv_(self): pass
+    def numexpr_(self): pass
+    def decnum_(self): pass
+    def neg_(self): pass
+    def pos_(self): pass
+    def int_(self): pass
+    def boolexpr_(self): pass
+    def logic_(self): pass
+    def cmpr_(self): pass
+    def stri_(self): pass
+    def c_(self): pass
+    def comment_(self): pass
+    def input_(self): pass
+    def output_(self): pass
+    def value_(self): pass
+
+    def text_(self):
+        if self.currentToken() ==
+
 
 def outputXML(ast, file):
     outFile = file.replace("inputs", "outputs")
@@ -74,3 +143,82 @@ def outputXML(ast, file):
     open(outFile, "w").write(xml)
 
 main("inputs")
+
+
+# PROGR ::= ALGO PROCDEFS
+# PROCDEFS ::= , PROC PROCDEFS
+# PROCDEFS ::= ''
+# PROC ::= p DIGITS { PROGR }
+# DIGITS ::= D MORE
+# D ::= 0
+# D ::= 1
+# D ::= 2
+# D ::= 3
+# D ::= 4
+# D ::= 5
+# D ::= 6
+# D ::= 7
+# D ::= 8
+# D ::= 9
+# MORE ::= DIGITS
+# MORE ::= ''
+# ALGO ::= INSTR COMMENT SEQ
+# SEQ ::= ; ALGO
+# SEQ ::= ''
+# INSTR ::= INPUT
+# INSTR ::= OUTPUT
+# INSTR ::= ASSIGN
+# INSTR ::= CALL
+# INSTR ::= LOOP
+# INSTR ::= BRANCH
+# INSTR ::= h
+# CALL ::= c p DIGITS
+# ASSIGN ::= NUMVAR := NUMEXPR 
+# ASSIGN ::= BOOLVAR := BOOLEXPR
+# ASSIGN ::= STRINGV := STRI
+# LOOP ::= w ( BOOLEXPR ) { ALGO } 
+# BRANCH ::= i ( BOOLEXPR ) t { ALGO } ELSE
+# ELSE ::= e { ALGO }
+# ELSE ::= ''
+# NUMVAR ::= n DIGITS
+# BOOLVAR ::= b DIGITS
+# STRINGV ::= s DIGITS
+# NUMEXPR ::= a ( NUMEXPR , NUMEXPR )
+# NUMEXPR ::= m ( NUMEXPR , NUMEXPR )
+# NUMEXPR ::= d ( NUMEXPR , NUMEXPR )
+# NUMEXPR ::= NUMVAR
+# NUMEXPR ::= DECNUM
+# DECNUM ::= 0.00
+# DECNUM ::= NEG
+# DECNUM ::= POS
+# NEG ::= ‒ POS
+# POS ::= INT . D D
+# INT ::= 1 MORE
+# INT ::= 2 MORE
+# INT ::= 3 MORE
+# INT ::= 4 MORE
+# INT ::= 5 MORE
+# INT ::= 6 MORE
+# INT ::= 7 MORE
+# INT ::= 8 MORE
+# INT ::= 9 MORE
+# BOOLEXPR ::= LOGIC
+# BOOLEXPR ::= CMPR
+# LOGIC ::= BOOLVAR
+# LOGIC ::= T
+# LOGIC ::= F
+# LOGIC ::= ^ ( BOOLEXPR , BOOLEXPR )
+# LOGIC ::= v ( BOOLEXPR , BOOLEXPR )
+# LOGIC ::= ! ( BOOLEXPR )
+# CMPR ::= E ( NUMEXPR , NUMEXPR )
+# CMPR ::= < ( NUMEXPR , NUMEXPR )
+# CMPR ::= > ( NUMEXPR , NUMEXPR )
+# STRI ::= " C C C C C C C C C C C C C C C "
+# C ::= as
+# COMMENT ::= * C C C C C C C C C C C C C C C *
+# COMMENT ::= ''
+# INPUT ::= g NUMVAR
+# OUTPUT ::= TEXT
+# OUTPUT ::= VALUE
+# VALUE ::= o NUMVAR
+# TEXT ::= r STRINGV
