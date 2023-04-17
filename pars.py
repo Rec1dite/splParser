4ba063b1f5a884904b9b7809040d20406032a33e
Rec1dite <rec1dite@gmail.com>
2023-04-17 03:33:29 +0200

    Implemented most of the parser functions

--- pars.py
+++ pars.py
@@ -69,31 +69,227 @@ class Parser:
         return { "token": token }
     
     def nNode(self, name, children):
-        return { name: children }
+        return {
+            name: {
+                "@id": str(self.index),
+                "@children": ",".join( [str(c["@id"]) for c in children] ),
+                **children
+            }
+        }
 
     #=============== GRAMMAR ===============#
     # Each of these returns a tree node
 
     def progr_(self):
-        if self.currentToken() in ["h", "g", "c", "w", "i", "r", "o", "n", "b", "s"]:
-            # ALGO PROCDEFS
+        if self.currentToken() in ["h", "g", "c", "w", "i", "r", "o", "n", "b", "s"]: # FIRST(PROGR)
+            # PROGR -> ALGO PROCDEFS
+            nodes = []
 
-            tree1 = algo_()
-            tree2 = procdefs_()
+            nodes.append(self.algo_())
 
-            tree = { "PROGR": self.currentToken() }
+            nodes.append(self.procdefs_())
 
-    def procdefs_(self): pass
-    def proc_(self): pass
-    def digits_(self): pass
-    def d_(self): pass
-    def more_(self): pass
-    def algo_(self): pass
-    def seq_(self): pass
-    def instr_(self): pass
+            return self.nNode("PROG", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected h, g, c, w, i, r, o, n, b or s")
+
+    def procdefs_(self):
+        if self.currentToken() == ",": # FIRST(PROCDEFS)
+            # PROCDEFS -> , PROC PROCDEFS
+            nodes = []
+
+            self.match(",")
+            nodes.append(self.tNode(","))
+
+            nodes.append(self.proc_())
+
+            nodes.append(self.procdefs_())
+
+            return self.nNode("PROCDEFS", nodes)
+        
+        elif self.currentToken() in ["$", "}"]: # FOLLOW(PROCDEFS)
+            # PROCDEFS -> epsilon
+
+            return self.nNode("PROCDEFS", [])
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected p or $")
+
+    def proc_(self):
+        if self.currentToken() == "p": # FIRST(PROC)
+            # PROC -> p DIGITS { PROGR }
+            nodes = []
+
+            self.match("p")
+            nodes.append(self.tNode("p"))
+
+            nodes.append(self.digits_())
+
+            self.match("{")
+            nodes.append(self.tNode("{"))
+
+            nodes.append(self.progr_())
+
+            self.match("}")
+            nodes.append(self.tNode("}"))
+
+            return self.nNode("PROC", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected p")
+
+    def digits_(self):
+        if self.currentToken() in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]: # FIRST(DIGITS)
+            # DIGITS -> D MORE
+            nodes = []
+
+            nodes.append(self.d_())
+
+            nodes.append(self.more_())
+
+            return self.nNode("DIGITS", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected 0-9")
+
+    def d_(self):
+        if self.currentToken() in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]: # FIRST(D)
+            # D -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+            nodes = []
+
+            # TODO: Check
+            # node = self.tNode(self.currentToken())
+            # self.match(self.currentToken())
+
+            # return node
+
+            self.match(self.currentToken())
+            nodes.append(self.tNode(self.currentToken()))
+
+            return self.nNode("D", nodes)
+
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected 0-9")
+
+    def more_(self):
+        if self.currentToken() in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]: # FIRST(MORE)
+            # MORE -> DIGITS
+            nodes = []
+
+            nodes.append(self.digits_())
+
+            return self.nNode("MORE", nodes)
+        
+        elif self.currentToken() in ["{", ".", ",", ";", "*", ":=", "}", "$", ")"]: # FOLLOW(MORE)
+            # MORE -> epsilon
+
+            return self.nNode("MORE", [])
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected 0-9, {, ., ,, ;, *, :=, }, $ or )")
+
+    def algo_(self):
+        if self.currentToken() in ["h", "g", "c", "w", "i", "r", "o", "n", "b", "s"]: # FIRST(ALGO)
+            # ALGO -> INSTR COMMENT SEQ
+            nodes = []
+
+            nodes.append(self.instr_())
+
+            nodes.append(self.comment_())
+
+            nodes.append(self.seq_())
+
+            return self.nNode("ALGO", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected h, g, c, w, i, r, o, n, b or s")
+
+    def seq_(self):
+        if self.currentToken() in [";"]: # FIRST(SEQ)
+            # SEQ -> ; ALGO
+            nodes = []
+
+            self.match(";")
+            nodes.append(self.tNode(";"))
+
+            nodes.append(self.algo_())
+
+            return self.nNode("SEQ", nodes)
+        
+        elif self.currentToken() in [",", "}", "$"]: # FOLLOW(SEQ)
+            # SEQ -> epsilon
+
+            return self.nNode("SEQ", [])
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected ;, , or }")
+
+    def instr_(self):
+        if self.currentToken() in ["g"]: # FIRST(INSTR)
+            # INSTR -> INPUT
+            nodes = []
+
+            nodes.append(self.input_())
+
+            return self.nNode("INSTR", nodes)
+        
+        elif self.currentToken() in ["o", "r"]: # FIRST(INSTR)
+            # INSTR -> OUTPUT
+            nodes = []
+
+            nodes.append(self.output_())
+
+            return self.nNode("INSTR", nodes)
+        
+        elif self.currentToken() in ["n", "b", "s"]: # FIRST(INSTR)
+            # INSTR -> ASSIGN
+            nodes = []
+
+            nodes.append(self.assign_())
+
+            return self.nNode("INSTR", nodes)
+        
+        elif self.currentToken() in ["c"]: # FIRST(INSTR)
+            # INSTR -> CALL
+            nodes = []
+
+            nodes.append(self.call_())
+
+            return self.nNode("INSTR", nodes)
+        
+        elif self.currentToken() in ["w"]: # FIRST(INSTR)
+            # INSTR -> LOOP
+            nodes = []
+
+            nodes.append(self.loop_())
+
+            return self.nNode("INSTR", nodes)
+        
+        elif self.currentToken() in ["i"]: # FIRST(INSTR)
+            # INSTR -> BRANCH
+            nodes = []
+
+            nodes.append(self.branch_())
+
+            return self.nNode("INSTR", nodes)
+        
+        elif self.currentToken() in ["h"]: # FIRST(INSTR)
+            # INSTR -> h
+            nodes = []
+
+            self.match("h")
+            nodes.append(self.tNode("h"))
+
+            return self.nNode("INSTR", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected g, o, r, n, b, s, c, w, i or h")
 
     def call_(self):
-        if self.currentToken() == "c":
+        if self.currentToken() == "c": # FIRST(CALL)
+            # CALL -> c p DIGITS
             nodes = []
 
             self.match("c")
@@ -109,20 +305,344 @@ class Parser:
         else:
             raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected c")
 
-    def assign_(self): pass
-    def loop_(self): pass
-    def branch_(self): pass
-    def else_(self): pass
-    def numvar_(self): pass
-    def boolvar_(self): pass
-    def stringv_(self): pass
-    def numexpr_(self): pass
-    def decnum_(self): pass
-    def neg_(self): pass
-    def pos_(self): pass
-    def int_(self): pass
-    def boolexpr_(self): pass
-    def logic_(self): pass
+    def assign_(self):
+        if self.currentToken() in ["n"]: # FIRST(ASSIGN)
+            # ASSIGN -> NUMVAR := NUMEXPR
+            nodes = []
+
+            nodes.append(self.numvar_())
+
+            self.match(":=")
+            nodes.append(self.tNode(":="))
+
+            nodes.append(self.numexpr_())
+
+            return self.nNode("ASSIGN", nodes)
+
+        elif self.currentToken() in ["b"]: # FIRST(ASSIGN)
+            # ASSIGN -> BOOLVAR := BOOLEXPR
+            nodes = []
+
+            nodes.append(self.boolvar_())
+
+            self.match(":=")
+            nodes.append(self.tNode(":="))
+
+            nodes.append(self.boolexpr_())
+
+            return self.nNode("ASSIGN", nodes)
+        
+        elif self.currentToken() in ["s"]: # FIRST(ASSIGN)
+            # ASSIGN -> STRINGV := STRI
+            nodes = []
+
+            nodes.append(self.stringv_())
+
+            self.match(":=")
+            nodes.append(self.tNode(":="))
+
+            nodes.append(self.stri_())
+
+            return self.nNode("ASSIGN", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected n, b or s")
+
+    def loop_(self):
+        if self.currentToken() == "w": # FIRST(LOOP)
+            # LOOP -> w ( BOOLEXPR ) { ALGO }
+            nodes = []
+
+            self.match("w")
+            nodes.append(self.tNode("w"))
+
+            self.match("(")
+            nodes.append(self.tNode("("))
+
+            nodes.append(self.boolexpr_())
+
+            self.match(")")
+            nodes.append(self.tNode(")"))
+
+            self.match("{")
+            nodes.append(self.tNode("{"))
+
+            nodes.append(self.algo_())
+
+            self.match("}")
+            nodes.append(self.tNode("}"))
+
+            return self.nNode("LOOP", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected w")
+
+    def branch_(self):
+        if self.currentToken() == "i": # FIRST(BRANCH)
+            # BRANCH -> i ( BOOLEXPR ) t { ALGO } ELSE
+            nodes = []
+
+            self.match("i")
+            nodes.append(self.tNode("i"))
+
+            self.match("(")
+            nodes.append(self.tNode("("))
+
+            nodes.append(self.boolexpr_())
+
+            self.match(")")
+            nodes.append(self.tNode(")"))
+
+            self.match("t")
+            nodes.append(self.tNode("t"))
+
+            self.match("{")
+            nodes.append(self.tNode("{"))
+
+            nodes.append(self.algo_())
+
+            self.match("}")
+            nodes.append(self.tNode("}"))
+
+            nodes.append(self.else_())
+
+            return self.nNode("BRANCH", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected i")
+
+    def else_(self):
+        if self.currentToken() == "e": # FIRST(ELSE)
+            # ELSE -> e { ALGO }
+            nodes = []
+
+            self.match("e")
+            nodes.append(self.tNode("e"))
+
+            self.match("{")
+            nodes.append(self.tNode("{"))
+
+            nodes.append(self.algo_())
+
+            self.match("}")
+            nodes.append(self.tNode("}"))
+
+            return self.nNode("ELSE", nodes)
+    
+        elif self.currentToken() in [",", ";", "*", "}", "$"]: # FOLLOW(ELSE)
+            # ELSE -> epsilon
+
+            return self.nNode("ELSE", [])
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected e, , or $")
+
+    def numvar_(self):
+        if self.currentToken() == "n": # FIRST(NUMVAR)
+            # NUMVAR -> n DIGITS
+            nodes = []
+
+            self.match("n")
+            nodes.append(self.tNode("n"))
+
+            nodes.append(self.digits_())
+
+            return self.nNode("NUMVAR", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected n")
+
+    def boolvar_(self):
+        if self.currentToken() == "b": # FIRST(BOOLVAR)
+            # BOOLVAR -> b DIGITS
+            nodes = []
+
+            self.match("b")
+            nodes.append(self.tNode("b"))
+
+            nodes.append(self.digits_())
+
+            return self.nNode("BOOLVAR", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected b")
+
+    def stringv_(self):
+        if self.currentToken() == "s": # FIRST(STRINGV)
+            # STRINGV -> s DIGITS
+            nodes = []
+
+            self.match("s")
+            nodes.append(self.tNode("s"))
+
+            nodes.append(self.digits_())
+
+            return self.nNode("STRINGV", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected s")
+
+    def numexpr_(self):
+        if self.currentToken() in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0.00", "-"]: # FIRST(NUMEXPR)
+            # NUMEXPR -> DECNUM
+            nodes = []
+
+            nodes.append(self.decnum_())
+
+            return self.nNode("NUMEXPR", nodes)
+
+        elif self.currentToken() == "n": # FIRST(NUMEXPR)
+            # NUMEXPR -> NUMVAR
+            nodes = []
+
+            nodes.append(self.numvar_())
+
+            return self.nNode("NUMEXPR", nodes)
+
+        elif self.currentToken() in ["a", "m", "d"]: # FIRST(NUMEXPR)
+            # NUMEXPR -> a ( NUMEXPR , NUMEXPR )
+            # NUMEXPR -> m ( NUMEXPR , NUMEXPR )
+            # NUMEXPR -> d ( NUMEXPR , NUMEXPR )
+            nodes = []
+
+            tok = self.currentToken()
+
+            self.match(tok)
+            nodes.append(self.tNode(tok))
+
+            self.match("(")
+            nodes.append(self.tNode("("))
+
+            nodes.append(self.numexpr_())
+
+            self.match(",")
+            nodes.append(self.tNode(","))
+
+            nodes.append(self.numexpr_())
+
+            self.match(")")
+            nodes.append(self.tNode(")"))
+
+            return self.nNode("NUMEXPR", nodes)
+
+        else:
+
+    def decnum_(self):
+        if self.currentToken() in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0.00", "-"]: # FIRST(DECNUM)
+            # DECNUM -> POS
+            nodes = []
+
+            nodes.append(self.pos_())
+
+            return self.nNode("DECNUM", nodes)
+
+        elif self.currentToken() == "0.00": # FIRST(DECNUM)
+            # DECNUM -> 0.00
+            nodes = []
+
+            self.match("0.00")
+            nodes.append(self.tNode("0.00"))
+
+            return self.nNode("DECNUM", nodes)
+
+        elif self.currentToken() == "-": # FIRST(DECNUM)
+            # DECNUM -> NEG
+            nodes = []
+
+            nodes.append(self.neg_())
+
+            return self.nNode("DECNUM", nodes)
+        
+        else:
+
+    def neg_(self):
+        if self.currentToken() == "-": # FIRST(NEG)
+            # NEG -> - POS
+            nodes = []
+
+            self.match("-")
+            nodes.append(self.tNode("-"))
+
+            nodes.append(self.pos_())
+
+            return self.nNode("NEG", nodes)
+        
+        else:
+
+
+    def pos_(self):
+        if self.currentToken() in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]: # FIRST(POS)
+            # POS -> INT . D D 
+            nodes = []
+
+            nodes.append(self.int_())
+
+            self.match(".")
+            nodes.append(self.tNode("."))
+
+            nodes.append(self.d_())
+
+            nodes.append(self.d_())
+
+            return self.nNode("POS", nodes)
+
+        else:
+
+    def int_(self):
+        if self.currentToken() in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]: # FIRST(INT)
+            # INT -> x MORE
+            nodes = []
+
+            v = self.currentToken()
+
+            self.match(v)
+            nodes.append(self.tNode(v))
+
+            nodes.append(self.more_())
+
+            return self.nNode("INT", nodes)
+
+        else:
+
+    def boolexpr_(self):
+        if self.currentToken() in ["b", "T", "F", "^", "v", "!"]: # FIRST(BOOLEXPR)
+            # BOOLEXPR -> LOGIC
+            nodes = []
+
+            nodes.append(self.logic_())
+
+            return self.nNode("BOOLEXPR", nodes)
+        
+        elif self.currentToken() in ["E", "<", ">"]: # FIRST(BOOLEXPR)
+            # BOOLEXPR -> CMPR
+            nodes = []
+
+            nodes.append(self.cmpr_())
+
+            return self.nNode("BOOLEXPR", nodes)
+        
+        else:
+
+    def logic_(self):
+        if self.currentToken() == "b": # FIRST(LOGIC)
+            # LOGIC -> BOOLVAR
+            nodes = []
+
+            nodes.append(self.boolvar_())
+
+            return self.nNode("LOGIC", nodes)
+
+        elif self.currentToken() in ["T", "F"]: # FIRST(LOGIC)
+            # LOGIC -> T | F
+            nodes = []
+
+            v = self.currentToken()
+
+            self.match(v)
+            nodes.append(self.tNode(v))
+
+            return self.nNode("LOGIC", nodes)
+
     def cmpr_(self): pass
     def stri_(self): pass
     def c_(self): pass
@@ -132,7 +652,20 @@ class Parser:
     def value_(self): pass
 
     def text_(self):
-        if self.currentToken() ==
+        if self.currentToken() == "r": # FIRST(TEXT)
+            # TEXT -> r STRINGV
+            nodes = []
+
+            self.match("r")
+            nodes.append(self.tNode("r"))
+
+            nodes.append(self.stringv_())
+
+            return self.nNode("TEXT", nodes)
+        
+        else:
+            raise Exception("Syntax error: unexpected " + self.currentToken() + ", expected r")
+
 
 
 def outputXML(ast, file):
